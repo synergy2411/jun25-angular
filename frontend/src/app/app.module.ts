8429e4746e7f09c68c3ff1e54a46dfe4d59d80d8
Sumit Khandelwal <synergy2411@outlook.com>
2025-06-05 12:22:18 +0530

    nested comp comm added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { UserImgComponent } from './components/users/user-img/user-img.component';
+import { UserInfoComponent } from './components/users/user-info/user-info.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersComponent],
+  declarations: [AppComponent, UsersComponent, UserImgComponent, UserInfoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/components/users/user-img/user-img.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user-img',
+  templateUrl: './user-img.component.html',
+  styleUrl: './user-img.component.css',
+})
+export class UserImgComponent {
+  @Input() user: any;
+
+  @Output()
+  childEvent = new EventEmitter();
+
+  onClick(user: any) {
+    this.childEvent.emit(user);
+  }
+}
